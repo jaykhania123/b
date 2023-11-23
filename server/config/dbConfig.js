b2757cb6ca11f13663bb52626e13265a32260905
jaykhania123 <132837435+jaykhania123@users.noreply.github.com>
2023-11-23 18:00:55 +0530

    Add files via upload

---
+++ server/config/dbConfig.js
@@ -0,0 +1,16 @@
+require('dotenv').config(); // Load environment variables from the .env file
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.on('connected', () => {
+    console.log('Mongo db connected successfully');
+});
+
+connection.on('error', (err) => {
+    console.log('Mongo db connection error: ', err);
+});
+
+module.exports = mongoose;
